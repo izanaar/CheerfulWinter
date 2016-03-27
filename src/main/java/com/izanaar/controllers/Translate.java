7a9347d4f84d5fbb812538352c726d04efeb627a
Ierus <theierus@gmail.com>
2016-03-27 22:24:42 +0300

    removed unnecessary dependencies

--- src/main/java/com/izanaar/controllers/Translate.java
+++ src/main/java/com/izanaar/controllers/Translate.java
@@ -2,20 +2,14 @@ package com.izanaar.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class Translate {
 
     @RequestMapping("/")
-    public void home(HttpServletResponse response) {
-        try {
-            response.getOutputStream().print("Hello World!");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public ModelAndView home() {
+        return new ModelAndView("translate");
     }
 
 }
