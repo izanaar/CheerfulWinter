eeca68b131034063727c79aae0d417778b96292c
Izanaar <izanaar@gmail.com>
2016-04-08 13:51:03 +0300

    started existing json API's research

--- src/main/java/com/izanaar/dao/DictionaryAPI.java
+++ src/main/java/com/izanaar/dao/DictionaryAPI.java
@@ -1,5 +1,8 @@
 package com.izanaar.dao;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -11,9 +14,15 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.PostConstruct;
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.net.URL;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
 
 @Service
 public class DictionaryAPI {
@@ -30,7 +39,7 @@ public class DictionaryAPI {
     private Map<String, Set<String>> directions;
 
     @PostConstruct
-    public void initTranslationDirections(){
+    public void initTranslationDirections() throws IOException {
         final String url = "https://dictionary.yandex.net/api/v1/dicservice.json/getLangs";
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(url)
             .queryParam("key", apiKey);
@@ -50,8 +59,25 @@ public class DictionaryAPI {
                             }
                     )
             );
+
+            lookup("text");
         } catch (RestClientException e) {
             e.printStackTrace();
         }
+    }
+
+    public void lookup(String text) throws IOException {
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                .queryParam("key", "2515")
+                .queryParam("lang", "en-ru")
+                .queryParam("text",text);
+        try {
+            String genreJson = IOUtils.toString(uriBuilder.build().toUri());
+            JSONObject json = new JSONObject(genreJson);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
