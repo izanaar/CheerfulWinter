fa6f2162c5e4e8e549ae10c8acffd929f420a0e3
repnikov <repnikov@megaputer.ru>
2016-11-01 12:26:31 +0300

    JCL -> SLF4J bridge configured logback as SLF4J implementation configured

--- src/main/java/com/izanaar/chwin/translate/service/LocalTranslateService.java
+++ src/main/java/com/izanaar/chwin/translate/service/LocalTranslateService.java
@@ -2,14 +2,20 @@ package com.izanaar.chwin.translate.service;
 
 import com.izanaar.chwin.translate.dto.TranslateRequest;
 import com.izanaar.chwin.translate.dto.Translation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LocalTranslateService implements TranslateService {
+
+    private static final Logger logger = LoggerFactory.getLogger(LocalTranslateService.class);
+
     public Translation translate(TranslateRequest translateRequest) {
         return null;
     }
 
     @Override
     public String toString() {
+        logger.info("Local TranslationService.toString() invoked.");
         return "Local TranslationService implementation";
     }
 }
